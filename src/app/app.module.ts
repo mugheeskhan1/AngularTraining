78f20932e4b56c58d92e825a340215597f7fd1c0
mughees-rt <mughees.khan@rolustech.net>
2022-09-23 18:19:27 +0500

    Added component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StringinterpolationComponent } from './stringinterpolation/stringinterpolation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StringinterpolationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/stringinterpolation/stringinterpolation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stringinterpolation',
+  templateUrl: './stringinterpolation.component.html',
+  styleUrls: ['./stringinterpolation.component.css']
+})
+export class StringinterpolationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
