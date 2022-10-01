65d270a873278eabe32418cc7e62f5d9d6d3b4ec
mugheeskhan1 <mugheeskhan36@gmail.com>
2022-10-01 13:09:56 +0500

    Hooks

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ContentChild, ElementRef, Input, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
+  //parent function can use this property because of @Input director
+  //  and we can also assign alias which which it can be accessed
+  @Input('srvElement') element!: { type: string, name: string, content: string };
+  @Input() name = '';
+  @ViewChild('heading', { static: true }) header!: ElementRef;
+  @ContentChild('contentParagraph', { static: true }) paragraph!: ElementRef;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor() {
+    console.log('constructor called!');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!');
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit called!');
+    console.log('Text Content: ' + this.header.nativeElement.textContent);
+    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent);
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called!');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called!');
+    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent);
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called!');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called!');
+    console.log('Text Content: ' + this.header.nativeElement.textContent);
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called!');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy called!');
   }
 
 }
