e4e3857db761707526b2288c741f177c2cb068e3
mugheeskhan1 <mugheeskhan36@gmail.com>
2022-10-04 15:55:39 +0500

    host listner implementation

--- src/app/dynamic_directive-binding/dynamic-directive-binding.directive.ts
+++ src/app/dynamic_directive-binding/dynamic-directive-binding.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appDynamicDirectiveBinding]'
 })
 export class DynamicDirectiveBindingDirective {
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+  
+  constructor() {}
 
-  constructor() { }
+  //so hostBinding also working like renderer but is easy to use
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'pink');
+    this.backgroundColor = 'pink';
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
+  }
 
 }
