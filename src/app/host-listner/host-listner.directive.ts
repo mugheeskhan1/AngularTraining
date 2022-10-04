69cdd3be17cdfe6e936248c63d5f2e43e9241dd1
mugheeskhan1 <mugheeskhan36@gmail.com>
2022-10-04 15:01:12 +0500

    HostListner: reactive directive: change color on hover

--- src/app/host-listner/host-listner.directive.ts
+++ src/app/host-listner/host-listner.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostBinding, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHostListner]'
@@ -7,6 +7,15 @@ export class HostListnerDirective {
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'pink');
+  }
+
+  //mouseenter and mouseleave are events supported by DOM 
+  @HostListener('mouseenter') mouseover(eventData: Event) {
     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'pink');
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
   }
 }
