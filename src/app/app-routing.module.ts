9f319b73ca349c52f074bf7c0d1dc3b560265878
mugheeskhan1 <mugheeskhan36@gmail.com>
2022-12-10 22:19:47 +0500

    Nested Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,24 @@ import { HomeComponent } from './home/home.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
 import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent },
-  { path: 'users/:id/:name', component: UsersComponent },
-  { path: 'servers', component: ServersComponent },
-  { path: 'servers/:id/edit', component: EditServerComponent },
-  { path: 'servers/:id', component: ServerComponent },
+  {
+    path: 'users',
+    component: UsersComponent,
+    children: [{ path: ':id/:name', component: UserComponent }],
+  },
+  {
+    path: 'servers',
+    component: ServersComponent,
+    children: [
+      { path: ':id/edit', component: EditServerComponent },
+      { path: ':id', component: ServerComponent },
+    ],
+  },
 ];
 
 @NgModule({
--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -8,12 +9,25 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class UserComponent implements OnInit {
   user!: { id: number; name: string };
-  constructor(private route: ActivatedRoute) {}
+  paramsSubscription: Subscription;
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
     this.user = {
       id: this.route.snapshot.params['id'],
-      name: this.route.snapshot.params['name'],
+      name: this.route.snapshot.params['name']
     };
+    this.paramsSubscription = this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }
+      );
+  }
+
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
   }
 }
