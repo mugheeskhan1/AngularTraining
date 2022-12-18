0951317ce8e9fd90aea2d82c74436794b86416b1
mugheeskhan1 <mugheeskhan36@gmail.com>
2022-12-19 00:18:28 +0500

    custom obseravable created

--- src/app/home1/home1.component.ts
+++ src/app/home1/home1.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Observer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home1',
   templateUrl: './home1.component.html',
-  styleUrls: ['./home1.component.css']
+  styleUrls: ['./home1.component.css'],
 })
-export class Home1Component implements OnInit {
-
-  constructor() { }
+export class Home1Component implements OnInit, OnDestroy {
+  private firstSubscription: Subscription;
+  constructor() {}
 
   ngOnInit(): void {
+    const customIntervalObserable = Observable.create((observer: any) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000);
+    });
+
+    this.firstSubscription = customIntervalObserable.subscribe((data: any) => {
+      console.log(data);
+    });
   }
 
+  ngOnDestroy(): void {
+    this.firstSubscription.unsubscribe();
+  }
 }
