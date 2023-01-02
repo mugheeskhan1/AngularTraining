9c485f4dd3e009c18e4c756c02bb4caa47dd5bbd
mugheeskhan1 <mugheeskhan36@gmail.com>
2023-01-02 20:54:12 +0500

    Using ngModel with Two-Way-Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   defaultQuestion = 'pet';
+  answer = '';
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
