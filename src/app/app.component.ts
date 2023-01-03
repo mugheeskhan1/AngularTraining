c3c3e9924e7ecfe03fd4b3e9fa09d39a529c6c05
mugheeskhan1 <mugheeskhan36@gmail.com>
2023-01-03 18:55:27 +0500

    setting up project for reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,48 +7,5 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  //access form
-  @ViewChild('f') signupForm: NgForm;
-
-  defaultQuestion = 'pet';
-  answer = '';
   genders = ['male', 'female'];
-  user = {
-    username: '',
-    email: '',
-    secret: '',
-    questionAnswer: '',
-    gender: '',
-  };
-  submitted = false;
-
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-
-    //Method 1:
-    //This approach will also update previous data in fields
-    // Good approach if we want to update all the fields of form
-
-    // this.signupForm.setValue({
-    //   userData: { username: suggestedName, email: '' },
-    //   secret: 'pet',
-    //   questionAnswer: '',
-    //   gender: 'male',
-    // });
-
-    //Method 2:
-    //better approach (only update value required)
-    this.signupForm.form.patchValue({ userData: { username: suggestedName } });
-  }
-
-  onSubmit(form: NgForm) {
-    this.submitted = true;
-    this.user.username = this.signupForm.value.userData.username;
-    this.user.email = this.signupForm.value.userData.email;
-    this.user.secret = this.signupForm.value.secret;
-    this.user.questionAnswer = this.signupForm.value.questionAnswer;
-    this.user.gender = this.signupForm.value.gender;
-
-    this.signupForm.reset();
-  }
 }
