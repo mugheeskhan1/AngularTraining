8ee21bc7d25c687dc597323725a6f58b00a3f80c
mugheeskhan1 <mugheeskhan36@gmail.com>
2023-01-03 11:40:41 +0500

    Setting and Patching Form Values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,11 +7,29 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  //access form
+  @ViewChild('f') signupForm: NgForm;
+
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
   suggestUserName() {
     const suggestedName = 'Superuser';
+
+    //Method 1:
+    //This approach will also update previous data in fields
+    // Good approach if we want to update all the fields of form
+
+    // this.signupForm.setValue({
+    //   userData: { username: suggestedName, email: '' },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male',
+    // });
+
+    //Method 2:
+    //better approach (only update value required)
+    this.signupForm.form.patchValue({ userData: { username: suggestedName } });
   }
 
   onSubmit(form: NgForm) {
